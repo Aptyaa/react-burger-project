40f4f6db85c42d769a1a911d48a8f65b902de66e
DShulga92@gmail.com <Dshulga92@gmail.com>
2024-09-15 17:09:22 +0400

    добавил редакс тулкит, сделал слайсы и rtk queryy api

---
+++ src/app/types.ts
@@ -0,0 +1,42 @@
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
+
+export interface IngredientsProp {
+	_id: string;
+	name: string;
+	type: string;
+	proteins: number;
+	fat: number;
+	carbohydrates: number;
+	calories: number;
+	price: number;
+	image: string;
+	image_mobile: string;
+	image_large: string;
+	__v: number;
+}
+
+export interface FetchedIngredients {
+	success: boolean;
+	data: IngredientsProp[];
+}
+
+export type IOrderConfirmRequest = string[];
+export interface IOrderConfirmResponse {
+	name: string;
+	order: {
+		number: number;
+	};
+	success: boolean;
+}
+
+export const isFetchBaseQueryErrorType = (
+	error: any
+): error is FetchBaseQueryError =>
+	(error as FetchBaseQueryError).status !== undefined;
+
+export interface IDropItemConstructor {
+	index: number;
+}
+export interface IDropItemIngredient {
+	id: string;
+}
